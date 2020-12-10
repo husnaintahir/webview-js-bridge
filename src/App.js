5b59c07e70697531e8a4070416834ee39ea640c7
Husnain Tahir <husnaintahir@gmail.com>
2020-12-10 23:08:46 +0500

    code push

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="container">
+
+
+          <button type="button" className="btn btn-primary" id="discoverPrinterBtn" >Click Me</button>
+
+        </div>
       </header>
     </div>
   );
